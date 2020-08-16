047d679bf118a094ea195d3f98c9bc5fd707aeac
Emmanuel Durand <shernua@gmail.com>
2020-08-15 21:31:53 -0500

    js file creation

---
+++ script.js
@@ -0,0 +1,175 @@
+//  click on button and go to the first question
+$(document).ready(function() {
+
+  $("#random-button").click(function() {
+
+    // remove all content of jumbotron once button is clicked
+    $("#TextIntro").remove();
+
+    var highScores = $("<p>").text("View HighScores");
+    $("body").append(highScores);
+    highScores.attr("id", "mainhighScores");
+
+
+
+    // add a new div with title
+    var newDivQuestion = $("<div>");
+    var newOl = $("<ol>");
+    
+    $(".jumbotron").append(newDivQuestion);
+    $(".jumbotron").append(newOl);
+    newDivQuestion.attr("style", "font-weight: bold");
+    newDivQuestion.attr("id", "mainDiv");
+    newDivQuestion.text("Commonly used data types DO NOT include:"); 
+
+    // add buttons
+    var button1 = $("<button>").text("1. strings"); 
+    var button2 = $("<button>").text("2. booleans");
+    var button3 = $("<button>").text("3. alerts");
+    var button4 = $("<button>").text("4. numbers");
+    
+    newOl.attr("id", "mainOl");     
+
+    var newHighScore = [];
+
+    // add success / fail message
+    var responseMessage = $("<p>").text(""); 
+    $(".jumbotron").append(responseMessage); 
+    responseMessage.attr("id", "mainParagraph");  
+
+    button1.attr("id", "firstButton");
+    button1.addClass("button");
+    button2.attr("id", "secondButton");
+    button2.addClass("button");
+    button3.attr("id", "thirdButton");
+    button3.addClass("button");
+    button4.attr("id", "fourthButton");
+    button4.addClass("button");
+    
+    $(newOl).append(button1, button2, button3, button4);
+
+    var questionNumber = 1;
+    var totalScore = 0;
+
+    function newQuestion(questionNumber) {
+      switch (questionNumber) {
+        case 2:
+          $(newDivQuestion).text("The condition in an if / else statement is enclosed within _______."); 
+          button1.text("1. quotes");
+          button2.text("2. curly brackets");
+          button3.text("3. parantheses"); 
+          button4.text("4. square brackets"); 
+          break;
+        case 3:
+        $(newDivQuestion).text("Arrays in JavaScript can be used to store _____."); 
+          button1.text("1. numbers and strings");
+          button2.text("2. other arrays");
+          button3.text("3. booleans"); 
+          button4.text("4. all of the above"); 
+          break;
+        case 4:
+        $(newDivQuestion).text("String values must be enclosed within ______ when being assigned to variables."); 
+          button1.text("1. commas");
+          button2.text("2. curly brackets");
+          button3.text("3. quotes"); 
+          button4.text("4. parentheses"); 
+          break;
+        case 5:
+        $(newDivQuestion).text("A very useful tool used during development and debugging for printing content to the debugger is:"); 
+          button1.text("1. JavaScript");
+          button2.text("2. terminal / bash");
+          button3.text("3. for loops"); 
+          button4.text("4. console.log"); 
+          break;
+        case 6:
+          allDone();
+          break;
+      }
+    };
+
+    function allDone() {
+      $("#mainOl").remove();
+      
+      $(newDivQuestion).text("All done!");
+      responseMessage.text("Your final score is " + totalScore + ".");
+      responseMessage.stop();
+
+      var enterInitials = $("<p>").text("Enter initials:"); 
+      $(".jumbotron").append(enterInitials); 
+
+      var inputBoxName = $("<input type=\"text\" id=\"fieldname\" />");
+      $(".jumbotron").append(inputBoxName); 
+      
+      var submitButton = $("<button>").text("Submit"); 
+      submitButton.attr("id", "buttonHS");
+      submitButton.addClass("buttonHighScore");
+      $(".jumbotron").append(submitButton); 
+
+      $(".buttonHighScore").on("click", function(event) {
+        console.log(inputBoxName.val);
+        HighScoresScreen(inputBoxName.text, totalScore);
+      });
+    }
+
+    $("#mainhighScores").on("click", function() {
+    alert("HighScores");
+    });
+
+    function HighScoresScreen(inputBoxName, totalScore){
+
+    }
+
+    $(".button").on("click", function(event) {
+      var buttonclicked = this.id;
+      
+
+      switch (buttonclicked){
+
+        case "firstButton":
+          responseMessage.text("Wrong!");
+          responseMessage.show();
+          responseMessage.fadeOut(600);  
+        break;
+
+        case "secondButton":
+          responseMessage.text("Wrong!");
+          responseMessage.show();
+          responseMessage.fadeOut(600); 
+        break;
+
+        case "thirdButton":
+          if (questionNumber===1 || questionNumber===2 || questionNumber===4)  {
+            responseMessage.text("Correct!");
+            totalScore++;
+            responseMessage.show();
+            responseMessage.fadeOut(600); 
+
+
+          }  else {
+            responseMessage.text("Wrong!");
+            responseMessage.show();
+            responseMessage.fadeOut(600); 
+          }
+        break;
+
+        case "fourthButton":
+        if (questionNumber===3 || questionNumber===5)  {
+          responseMessage.text("Correct!");
+          totalScore++;
+          responseMessage.show();
+          responseMessage.fadeOut(600); 
+          }  else {
+          responseMessage.text("Wrong!");
+          responseMessage.show();
+          responseMessage.fadeOut(600); 
+          }
+        break;  
+      } 
+      
+      questionNumber++;
+      newQuestion(questionNumber);
+    });
+
+  });
+
+});
